7cb694a266c7eb2e189167946cf0dc9684805b5b
Matt Lopez <matthew.lopez4011@gmail.com>
2020-01-22 23:27:12 -0600

    add

--- js/apiKeys.js
+++ js/apiKeys.js
@@ -1,2 +1,2 @@
-// var mapBoxToken = 'pk.eyJ1IjoibWF0dDM2MDciLCJhIjoiY2p6aWhjYWFyMTZucDNjcTZxYnhjaTRjNyJ9.RegX-B-HGNFCIAH3vShlyw';
+var mapBoxToken = 'pk.eyJ1IjoibWF0dDM2MDciLCJhIjoiY2p6aWhjYWFyMTZucDNjcTZxYnhjaTRjNyJ9.RegX-B-HGNFCIAH3vShlyw';
 let you = 'Your mom!';
